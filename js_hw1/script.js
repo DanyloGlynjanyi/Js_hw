f76633e98124cc7a6f946735f5cdc59acd2192f0
test_1 <dglynjanyi001@icloud.com>
2025-02-08 19:50:41 +0200

    Add js homework 1.

--- js_hw1/script.js
+++ js_hw1/script.js
@@ -33,6 +33,7 @@ let firstName = "Vitalik"
 let middleName = "Olegovich"
 let lastName = "Alekseev"
 let person = firstName + " " + middleName + " " + lastName
+
 console.log(person);
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 
