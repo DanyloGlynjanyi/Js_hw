a77967b99e80fd496a010c74019f75f651a8220a
test_1 <dglynjanyi001@icloud.com>
2025-02-11 13:40:33 +0200

    Add JavaScript homework 4.

--- js_hw4/main.js
+++ js_hw4/main.js
@@ -160,7 +160,7 @@ swap([11, 22, 33, 44], 0, 1);
 function exchange(sumUAH, currencyValues, exchangeCurrency) {
     for (const item of currencyValues) {
         sum1 = sumUAH
-        if (exchangeCurrency === `${item.currency}`) {
+        if (exchangeCurrency === item.currency) {
             res= sum1 / `${item.value}`
             console.log(`${item.currency}:${res}`)
         }
